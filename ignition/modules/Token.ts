8626158eb083efd9b6a513186a79a214c97d6bef
Nacho Colomina <ignacio.colomina@gmail.com>
2024-09-25 17:05:26 +0200

    Fix Token ignition module name

--- ignition/modules/Token.ts
+++ ignition/modules/Token.ts
@@ -1,6 +1,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-export default buildModule("Asset", (m) => {
+export default buildModule("Token", (m) => {
   const token = m.contract("HouseTestTokenERC20", ['My Token', 'MTY']);
   return { token };
 });
